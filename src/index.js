2569442dadebd0eb69745ee31145732e8fa21740
Andrew Carroll <f.andrew.carroll@gmail.com>
2017-11-19 14:17:39 -0800

    able to pull comment totals and output all data in cli

--- src/index.js
+++ src/index.js
@@ -26,6 +26,17 @@ const optionDefinitions = [
 // Setting number patter for removing the letter from the period request
 const numberPattern = /\d+/g
 
+// function to count number of times something exists in an array
+function countInArray(array, what) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === what) {
+      count++
+    }
+  }
+  return count
+}
+
 // Parsing the options from the command line and assign to variables
 const options = commandLineArgs(optionDefinitions)
 const repo = options.repo
@@ -46,7 +57,7 @@ const timer = setInterval(function() {
   }
 }, 100)
 
-async function printComments() {
+async function printCommentData() {
   try {
     // GET commit comments and store user names
     const commitsUsers = await http
@@ -61,6 +72,7 @@ async function printComments() {
         // count number of comments user made on all of the repo
         return users
       })
+
     // GET issues comments and store user names
     const issuesUsers = await http
       .get(`/repos/${repo}/issues/comments?per_page=100`)
@@ -74,6 +86,7 @@ async function printComments() {
         // count number of comments user made on all of the repo
         return users
       })
+
     // GET pull request comments and store user names
     const pullsUsers = await http
       .get(`/repos/${repo}/pulls/comments?per_page=100`)
@@ -87,16 +100,18 @@ async function printComments() {
         // count number of comments user made on all of the repo
         return users
       })
-    // comments is an array with all of the instances a name shows up from comments
-    const comments = commitsUsers + issuesUsers + pullsUsers
-    console.log(comments)
-    // GET stats of the contributors and store as object
+
+    // comments array with all of the instances a name shows up from comments
+    const comments = commitsUsers.concat(issuesUsers, pullsUsers)
+
+    // GET number contributors and store as object in an array called stats
     // GET /repos/:owner/:repo/stats/contributors
     const stats = await http
       .get(`/repos/${repo}/stats/contributors`)
       .then(res => {
         let users = new Array()
         res.data.forEach(function(contributor) {
+          // define a user object to go into the users array
           let user = {
             name: contributor.author.login,
             totalCommits: Number(contributor.total),
@@ -108,18 +123,16 @@ async function printComments() {
         users.sort(function(a, b) {
           return parseFloat(b.totalCommits) - parseFloat(a.totalCommits)
         })
-        // return user object to be saved as const stats
+        // return users array of user objects to be saved as const stats
         return users
-        // let users = {}
-        // console.log(res.data[0].total, res.data[0].author)
       })
-    // console.log(stats)
+
+    // loop over the comments and compare to users to determine totalComments
     for (let user of stats) {
-      for (let comment of comments) {
-        
-      }
+      user.totalComments = countInArray(comments, user.name)
       console.log(
-        `${leftPad(user.totalCommits, 4)} comments, ${user.name} ( commits)`,
+        `${leftPad(user.totalCommits, 4)} comments, ${user.name}`,
+        `(${user.totalComments} commits)`,
       )
     }
   } catch (err) {
@@ -128,4 +141,4 @@ async function printComments() {
   }
 }
 
-printComments()
+printCommentData()
