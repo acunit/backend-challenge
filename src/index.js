50bf5057197ca678faff6a43f5bcb06fe241748e
Andrew Carroll <f.andrew.carroll@gmail.com>
2017-11-18 22:10:14 -0800

    first line of command-line response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+const apiBase = 'https://api.github.com'
+
+// library for parsing command line options
+const commandLineArgs = require('command-line-args')
+const axios = require('axios')
 const chalk = require('chalk')
 const config = require('./config')
+const http = axios.create({
+  baseURL: apiBase,
+  headers: {
+    Authorization: `token ${config.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+})
 
-console.log(chalk.yellow('Your github token is:'))
-console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
+// Defining the command line arguments
+const optionDefinitions = [
+  { name: 'repo', alias: 'r', type: String },
+  { name: 'period', type: String },
+]
 
+// Setting number patter for removing the letter from the period request
+const numberPattern = /\d+/g
 
-// remove this line
-require('./example')
+// Parsing the options from the command line and assign to variables
+const options = commandLineArgs(optionDefinitions)
+const repo = options.repo
+const period = !options.period
+  ? 'all comments'
+  : `comments for past ${options.period.match(numberPattern)[0]} day(s)`
+
+// Log request with the parameters specified
+console.log(`  Fetching ${period} for "${repo}"...`)
